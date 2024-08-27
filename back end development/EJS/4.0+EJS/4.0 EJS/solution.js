0bfab452930252a04c083335feb4ffdd7684984e
Sudeep-07 <sudeepaa3007@gmail.com>
2024-08-28 01:41:18 +0530

    ejs update

--- back end development/EJS/4.0+EJS/4.0 EJS/solution.js
+++ back end development/EJS/4.0+EJS/4.0 EJS/solution.js
@@ -4,29 +4,24 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  const today = new Date();
-  //Test code
-  // weekend:
-  // new Date("June 24, 2023 11:13:00");
-  // weekday:
-  // new Date("June 20, 2023 11:13:00");
-  const day = today.getDay();
 
-  // console.log(day);
-  let type = "a weekday";
-  let adv = "it's time to work hard";
+const today = new Date();
+const day = today.getDate();
 
-  if (day === 0 || day === 6) {
-    type = "the weekend";
-    adv = "it's time to have some fun";
-  }
+let type = "a weekday";
+let adv = "it's time to work hark";
+
+if(day === 0 || day === 6) {
+   type = "the weekend";
+   adv = "it's time to have fun ";
+}
 
   res.render("solution.ejs", {
-    dayType: type,
+    daytype: type,
     advice: adv,
   });
 });
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}.`);
+  console.log(`server running on port ${port}. `);
 });
