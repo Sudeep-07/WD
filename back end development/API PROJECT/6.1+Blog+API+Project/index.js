d111198dec260c9efb28a712e1ed342cd19a4cce
Sudeep-07 <sudeepaa3007@gmail.com>
2024-09-01 02:07:38 +0530

    new own api

--- back end development/API PROJECT/6.1+Blog+API+Project/index.js
+++ back end development/API PROJECT/6.1+Blog+API+Project/index.js
@@ -35,63 +35,56 @@ let posts = [
 let lastId = 3;
 
 // Middleware
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-//Write your code here//
-
-//CHALLENGE 1: GET All posts
-
+// GET all posts
 app.get("/posts", (req, res) => {
   console.log(posts);
   res.json(posts);
 });
 
-//CHALLENGE 2: GET a specific post by id
-
+// GET a specific post by id
 app.get("/posts/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const foundPost = posts.find((post) => post.id === id);
-  if(!foundPost) return res.status(404).json({ message: "post not found"});
-  res.json(foundPost);
+  const post = posts.find((p) => p.id === parseInt(req.params.id));
+  if (!post) return res.status(404).json({ message: "Post not found" });
+  res.json(post);
 });
 
-//CHALLENGE 3: POST a new post
-
+// POST a new post
 app.post("/posts", (req, res) => {
   const newId = lastId += 1;
   const post = {
     id: newId,
-    title: req.body.content,
+    title: req.body.title,
+    content: req.body.content,
     author: req.body.author,
     date: new Date(),
   };
   lastId = newId;
   posts.push(post);
-  req.status(201).json(post);
+  res.status(201).json(post);
 });
-//CHALLENGE 4: PATCH a post when you just want to update one parameter 
 
+// PATCH a post when you just want to update one parameter
 app.patch("/posts/:id", (req, res) => {
   const post = posts.find((p) => p.id === parseInt(req.params.id));
-  if(!post) return res.status(404).json({ message: "post not found"});
+  if (!post) return res.status(404).json({ message: "Post not found" });
 
-  if(req.body.title) post.title = req.body.title;
-  if(req.body.content) post.content = req.body.content;
-  if(req.body.author) post.author = req.body.author;
+  if (req.body.title) post.title = req.body.title;
+  if (req.body.content) post.content = req.body.content;
+  if (req.body.author) post.author = req.body.author;
 
   res.json(post);
 });
 
-
-//CHALLENGE 5: DELETE a specific post by providing the post id.
-
-app.delete("/post/:id", (req, res) => {
+// DELETE a specific post by providing the post id
+app.delete("/posts/:id", (req, res) => {
   const index = posts.findIndex((p) => p.id === parseInt(req.params.id));
-  if (index === -1) return res.status(404).json({ message: "post not found"});
-  
+  if (index === -1) return res.status(404).json({ message: "Post not found" });
+
   posts.splice(index, 1);
-  res.json({ message: "post deleted"});
+  res.json({ message: "Post deleted" });
 });
 
 app.listen(port, () => {
