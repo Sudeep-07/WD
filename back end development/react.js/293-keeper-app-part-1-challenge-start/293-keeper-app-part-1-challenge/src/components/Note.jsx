9204a1cabd345b87595cea3d9c8f0d1b6ac12af4
Sudeep-07 <sudeepaa3007@gmail.com>
2024-09-19 13:24:52 +0530

    react keeper part 1 complete

---
+++ back end development/react.js/293-keeper-app-part-1-challenge-start/293-keeper-app-part-1-challenge/src/components/Note.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+function Note() {
+    return(
+        <div className="note">
+            <h1>
+                this is a title
+            </h1>
+            <p>
+                this is a content
+            </p>
+        </div>
+    );
+}
+
+export default Note;
