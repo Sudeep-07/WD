e2c804e64e1d1c780766cfabc8852e2d7e38c28a
Sudeep-07 <sudeepaa3007@gmail.com>
2024-09-21 03:39:42 +0530

    keeper part 2

--- back end development/react.js/302-keeper-app-part-2-start/302-keeper-app-part-2-starting/src/components/App.jsx
+++ back end development/react.js/302-keeper-app-part-2-start/302-keeper-app-part-2-starting/src/components/App.jsx
@@ -2,12 +2,21 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
+
+// Corrected the createNotes function to properly return JSX
+
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map((noteItem) => 
+    <Note
+      key={noteItem.id}
+      title={noteItem.title}
+      content={noteItem.content}
+    />)}
       <Footer />
     </div>
   );
